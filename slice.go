b94eb50bdb46535486f1cb13f0a378228aeed3b8
toshitk <takahashi.t66@gmail.com>
2024-03-26 12:04:55 +0900

    feat: slices

--- slice.go
+++ slice.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
-	var x = []int{10, 20, 30}
+	var x = []int{10, 20, 30, 40, 50}
+	fmt.Println(x)
+
+	// delete x[1], x[2] (delete 20, 30)
+	x = append(x[:1], x[3:]...)
+	fmt.Println(x)
+
+	var y = []int{10, 20, 30, 40, 50}
+	// delete x[1], x[2] (delete 20, 30)
+	y = slices.Delete(y, 1, 3)
+	fmt.Println(y)
+
+	var z = []int{1, 2, 3, 4, 5}
+	x = slices.Insert(x, 1, z[1:3]...)
 	fmt.Println(x)
 }
